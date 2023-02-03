83d8f2edd6abafde161f5c4b1bcb342473ceb1eb
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-03 10:36:33 -1000

    fix /home route to /

--- pages/recurse/index.tsx
+++ pages/recurse/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function index() {
   return (
     <div className={"p-4 dark:bg-zinc-800 h-screen"}>
-      <div className={"pb-4 text-2xl"}><Link className={"no-underline hover:underline"} href="/home">Cindy Wu</Link> › Recurse</div>
+      <div className={"pb-4 text-2xl"}><Link className={"no-underline hover:underline"} href="/">Cindy Wu</Link> › Recurse</div>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/minimal-linkedin"><div>minimal linkedin</div></Link>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/uses-toy"><div>/uses</div></Link>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/baby-crdt"><div>baby crdt v1 - insertions</div></Link>
