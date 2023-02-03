216b819376a7a283a7170ed475bfa14c65b6e447
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-03 10:35:55 -1000

    update /recurse

--- pages/recurse/index.tsx
+++ pages/recurse/index.tsx
@@ -7,8 +7,11 @@ export default function index() {
       <div className={"pb-4 text-2xl"}><Link className={"no-underline hover:underline"} href="/home">Cindy Wu</Link> › Recurse</div>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/minimal-linkedin"><div>minimal linkedin</div></Link>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/uses-toy"><div>/uses</div></Link>
-      <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/baby-crdt"><div>baby crdt</div></Link>
+      <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/baby-crdt"><div>baby crdt v1 - insertions</div></Link>
+      <Link className={"no-underline hover:underline"} href="/arrows-2"><div>baby crdt v2 - document array of chars </div></Link>
       <Link className={"no-underline hover:underline"} href="/arrows"><div>arrows</div></Link>
+      <Link className={"no-underline hover:underline"} href="/tailwind-desktop"><div>tailwindcss desktop app - mac/windows/linux</div></Link>
+      <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/baby-crdt/pull/14"><div>baby crdt v3 - tombstoning some chars to heaven</div></Link>
     </div>
   )
 }
