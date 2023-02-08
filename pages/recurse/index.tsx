1f302604e8dbaea906394ecdb1f2dfa096ae15c2
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-07 21:17:36 -1000

    more info

--- pages/recurse/index.tsx
+++ pages/recurse/index.tsx
@@ -6,10 +6,10 @@ export default function index() {
     <div className={"p-4 dark:bg-zinc-800 h-screen"}>
       <div className={"pb-4 text-2xl"}><Link className={"no-underline hover:underline"} href="/">Cindy Wu</Link> › Recurse</div>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/minimal-linkedin"><div>minimal linkedin</div></Link>
-      <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/uses-toy"><div>/uses</div></Link>
+      <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/uses-toy"><div>/uses - what you use determines who you are</div></Link>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/baby-crdt"><div>crdt from scratch v1 - insertions</div></Link>
       <Link className={"no-underline hover:underline"} href="/arrows-2"><div>crdt from scratch v2 - document array of chars </div></Link>
-      <Link className={"no-underline hover:underline"} href="/arrows"><div>arrows</div></Link>
+      <Link className={"no-underline hover:underline"} href="/arrows"><div>arrows - public community-run wayback machine for anything you care about</div></Link>
       <Link className={"no-underline hover:underline"} href="/tailwind-desktop"><div>tailwindcss desktop app - mac/windows/linux</div></Link>
       <Link className={"no-underline hover:underline"} href="https://github.com/cindywu/baby-crdt/pull/14"><div>crdt from scratch v3 - tombstoning some chars to heaven</div></Link>
       <div>spin and stop and bounce<span className={"text-yellow-400"}>{` - coming soon!`}</span></div>
