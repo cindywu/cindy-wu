6a7d45d623356f74b2bfae78c6934b05a5d93599
cindy <1177031+cindywu@users.noreply.github.com>
2023-03-03 07:46:51 -1000

    explanation

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -4,6 +4,9 @@ export default function index() {
   return (
     <div className={"p-4 dark:bg-zinc-800 h-screen overflow-auto"}>
     <div className={"text-2xl pb-4"}>Friends acquantainces strangers who are hiring</div>
+    <div className={"py-2 w-1/2"}>It is very unlikely that I will end up at a startup, but these exceptional humans are fighting the good fight I want my friends to go write code for them. Some engineering management mixed in, also not my jam at this point in life.</div>
+    <div>Cindy</div>
+    <div>--</div>
     <div>
       Alice Albrecht
     </div>
