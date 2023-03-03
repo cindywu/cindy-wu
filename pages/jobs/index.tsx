4e680bae1f1a9a2c864c1d0cd950a6d38d635e63
cindy <1177031+cindywu@users.noreply.github.com>
2023-03-03 07:37:24 -1000

    interesting jobs that i probably won't do

---
+++ pages/jobs/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div className={"p-4 dark:bg-zinc-800 h-screen overflow-auto"}>
+    <div className={"text-2xl pb-4"}>Friends acquantainces strangers who are hiring</div>
+    <div>
+      Alice Albrecht
+    </div>
+    <div>
+      Dhash Shrivathsa
+    </div>
+    <div>
+      Kishan Bagaria
+    </div>
+    <div>
+      Pranav Piyush
+    </div>
+    <div>
+      Jungwon Byun
+    </div>
+    <div>Andreas Stuhlmüller</div>
+    <div>
+      Jordan Scales
+    </div>
+    <div>
+      Jason Hoch
+    </div>
+    <div>
+      Purya Sarmadi
+    </div>
+    <div>
+      Amit Gupta
+    </div>
+    </div>
+  )
+}
