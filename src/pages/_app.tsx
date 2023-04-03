cbe3446fcdb82a901382132f41afbbf06bbf7ae9
cindy <1177031+cindywu@users.noreply.github.com>
2023-04-02 21:54:39 -1000

    remove analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react'
+// import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      {/* <Analytics /> */}
     </>
   )
 }
